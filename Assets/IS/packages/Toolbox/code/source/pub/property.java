0e6d8ad4a75be016fc37f8017a426021202770d6
Biggers <biggersbb@ncias-d2289-c.nci.nih.gov>
2021-12-28 09:18:31 -0500

    Sprint 23, JIRA tickets 6, 1441, 1421, 1415

--- Assets/IS/packages/Toolbox/code/source/pub/property.java
+++ Assets/IS/packages/Toolbox/code/source/pub/property.java
@@ -10,7 +10,9 @@ import com.wm.app.b2b.server.ServiceException;
 import com.wm.app.b2b.server.ServerAPI;
 import java.util.Properties;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 // --- <<IS-END-IMPORTS>> ---
 
@@ -55,6 +57,41 @@ public final class property
 		}
 		pipelineCursor.destroy();
 			
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void setPropertyValue (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(setPropertyValue)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required packageName
+		// [i] field:0:required propertyFileName
+		// [i] field:0:required property
+		// [i] field:0:required propertyValue
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		String	pkgname = IDataUtil.getString( pipelineCursor, "packageName" );
+		String  propertyFileName = IDataUtil.getString(pipelineCursor,"propertyFileName");
+		String	property = IDataUtil.getString( pipelineCursor, "property" );
+		String  propertyValue = IDataUtil.getString(pipelineCursor, "propertyValue" );
+		try{
+			InputStream is = new FileInputStream(ServerAPI.getPackageConfigDir(pkgname).toString()+"/"+propertyFileName);
+			Properties prop = new Properties();
+			prop.load(is);
+			is.close();
+			OutputStream os = new FileOutputStream(ServerAPI.getPackageConfigDir(pkgname).toString()+"/"+propertyFileName);
+			prop.setProperty(property, propertyValue);
+			prop.store(os, "");
+			os.close();
+		} catch (Exception e){
+			//IDataUtil.put( pipelineCursor, "msg", e.toString() );
+			throw new ServiceException(e.toString());
+		}
+		pipelineCursor.destroy();
 		// --- <<IS-END>> ---
 
                 
